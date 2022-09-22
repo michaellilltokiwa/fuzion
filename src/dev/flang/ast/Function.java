a07df2be5fb04e311c481298b4fcd6d85dfe8d01
Michael Lill <michael.lill@tokiwa.software>
2022-09-22 14:40:28 +0200

    remove system.exit(1)

--- src/dev/flang/ast/Function.java
+++ src/dev/flang/ast/Function.java
@@ -212,7 +212,6 @@ public class Function extends ExprWithPos
                 if (f.isConstructor())
                   {
                     System.err.println("NYI: fun for constructor type not allowed");
-                    System.exit(1);
                   }
               }
           }
